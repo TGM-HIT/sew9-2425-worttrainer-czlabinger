3781da9b0bd45667aafc14652590bec1234c862a
czlabinger <czlabinger@student.tgm.ac.at>
2024-09-16 21:46:35 +0200

    Added Controller constructor

--- src/main/java/at/czlabinger/Controller.java
+++ src/main/java/at/czlabinger/Controller.java
@@ -1,7 +1,17 @@
 package at.czlabinger;
 
+import at.czlabinger.model.WortTrainer;
+
 public class Controller {
     public static void main(String[] args) {
+        Controller c = new Controller();
+    }
 
+    private View v;
+    private WortTrainer wt;
+
+    public Controller() {
+        this.v = new View();
+        this.wt = new WortTrainer(new String[]{"Hund"}, new String[]{"https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg"});
     }
 }
