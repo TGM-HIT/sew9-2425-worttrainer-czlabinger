2d463ef261542e88c93f7b82cb4983d0d323ef7c
czlabinger <czlabinger@student.tgm.ac.at>
2024-09-18 14:34:25 +0200

    Added unit tests

--- src/test/java/at/czlabinger/AppTest.java
+++ src/test/java/at/czlabinger/AppTest.java
@@ -1,25 +1,39 @@
 package at.czlabinger;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import at.czlabinger.model.SaveLoadHelper;
 import at.czlabinger.model.WortTrainer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class AppTest {
 
+    private WortTrainer wt;
+
+    @BeforeEach
+    public void setup() {
+        wt = new WortTrainer(new String[]{"Haus"}, new String[]{"https://www.haus.at"});
+    }
+
     @Test
     public void checkAnswer() {
-        WortTrainer wt = new WortTrainer(new String[]{"Haus"}, new String[]{"haus.at"});
         assertTrue(wt.checkSelectedWord("Haus"));
         assertFalse(wt.checkSelectedWord("Fasd"));
     }
 
     @Test
     public void checkAnswerIgnoreCase() {
-        WortTrainer wt = new WortTrainer(new String[]{"Haus"}, new String[]{"haus.at"});
         assertTrue(wt.checkSelectedWordIgnoreCase("haus"));
         assertFalse(wt.checkSelectedWordIgnoreCase("Fasd"));
+    }
+
+    @Test
+    public void saveLoadTest() {
+        SaveLoadHelper.save(wt, "/home/stoffi05/Downloads/worttrainer.save");
+        wt.checkSelectedWord("ada");
+        WortTrainer loaded = SaveLoadHelper.load("/home/stoffi05/Downloads/worttrainer.save");
+        assertNotEquals(wt, loaded);
     }
 }
