634e9f8173321451e133c42421c14e0e07b80524
czlabinger <czlabinger@student.tgm.ac.at>
2024-09-18 16:46:31 +0200

    Fixed if no save file exists

--- src/main/java/at/czlabinger/model/SaveLoadHelper.java
+++ src/main/java/at/czlabinger/model/SaveLoadHelper.java
@@ -17,11 +17,13 @@ public class SaveLoadHelper {
     }
 
     public static WortTrainer load(String path) {
+
         try (ObjectInputStream is = new ObjectInputStream(new FileInputStream(path))) {
             return (WortTrainer) is.readObject();
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
+        } catch (IOException e) {
+            return new WortTrainer(new WortListe(new String[]{"Dog", "Cat"}, new String[]{"https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/800px-Cat_November_2010-1a.jpg"}));
         }
     }
-
 }
