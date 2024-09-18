bbb5b4a7dee37e7b6b0a01ec94f4ee708a13f41d
czlabinger <czlabinger@student.tgm.ac.at>
2024-09-18 16:28:59 +0200

    Fixed creation of Worttrainer

--- src/main/java/at/czlabinger/Controller.java
+++ src/main/java/at/czlabinger/Controller.java
@@ -1,6 +1,7 @@
 package at.czlabinger;
 
 import at.czlabinger.model.SaveLoadHelper;
+import at.czlabinger.model.WortListe;
 import at.czlabinger.model.WortTrainer;
 import at.czlabinger.view.View;
 
@@ -16,7 +17,7 @@ public class Controller {
         try {
             this.wt = SaveLoadHelper.load("/home/stoffi05/Downloads/worttrainer.save");
         } catch (RuntimeException e) {
-            this.wt = new WortTrainer(new String[]{"Dog", "Cat"}, new String[]{"https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/800px-Cat_November_2010-1a.jpg"});
+            this.wt = new WortTrainer(new WortListe(new String[]{"Dog", "Cat"}, new String[]{"https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/800px-Cat_November_2010-1a.jpg"}));
         }
         this.v = new View(this);
     }
