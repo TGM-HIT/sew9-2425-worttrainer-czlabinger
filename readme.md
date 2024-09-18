d3c50ef00c29cf6935d179ae6762683ce74fea4e
czlabinger <czlabinger@student.tgm.ac.at>
2024-09-18 14:54:16 +0200

    Changed language to english and updated readme

--- readme.md
+++ readme.md
@@ -1,2 +1,26 @@
 # Worttrainer
 
+## Dependencies
+
+* Jdk21
+* maven
+
+## Installation
+```bash
+# Clone Project
+git clone https://github.com/TGM-HIT/sew9-2425-worttrainer-czlabinger.git
+cd sew9-2425-worttrainer-czlabinger
+
+# Build jar
+mvn clean package -DskipTests
+
+# Run jar
+java -jar target/worttrainer-1.0-SNAPSHOT.jar
+```
+
+## Usage
+
+After sarting the jar a window will appear with the picture of a dog or a cat. 
+In the given text field either type 'Cat' or 'Dog' depending on what the picture shows. 
+In the bottom left are stats for how many inputs you did and how many were correct. 
+If you want to add a new word in the text field type: 'wordYouWantToAdd, directLinkToImage' and press 'Add Word'.
--- src/main/java/at/czlabinger/Controller.java
+++ src/main/java/at/czlabinger/Controller.java
@@ -16,7 +16,7 @@ public class Controller {
         try {
             this.wt = SaveLoadHelper.load("/home/stoffi05/Downloads/worttrainer.save");
         } catch (RuntimeException e) {
-            this.wt = new WortTrainer(new String[]{"Hund", "Katze"}, new String[]{"https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/800px-Cat_November_2010-1a.jpg"});
+            this.wt = new WortTrainer(new String[]{"Dog", "Cat"}, new String[]{"https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/800px-Cat_November_2010-1a.jpg"});
         }
         this.v = new View(this);
     }
@@ -51,7 +51,7 @@ public class Controller {
     }
 
     public void newGame() {
-        this.wt = new WortTrainer(new String[]{"Hund"}, new String[]{"https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg"});
+        this.wt = new WortTrainer(new String[]{"Dog"}, new String[]{"https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg"});
         this.v.update();
     }
 
