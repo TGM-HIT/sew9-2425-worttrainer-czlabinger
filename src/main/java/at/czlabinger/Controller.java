b1e47ac7c94a2f1904b63b6d390f14e390804212
czlabinger <czlabinger@student.tgm.ac.at>
2024-09-18 16:29:20 +0200

    Fixed creation of Worttrainer

--- src/main/java/at/czlabinger/Controller.java
+++ src/main/java/at/czlabinger/Controller.java
@@ -52,7 +52,7 @@ public class Controller {
     }
 
     public void newGame() {
-        this.wt = new WortTrainer(new String[]{"Dog"}, new String[]{"https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg"});
+        this.wt = new WortTrainer(new WortListe(new String[]{"Dog", "Cat"}, new String[]{"https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/800px-Cat_November_2010-1a.jpg"}));
         this.v.update();
     }
 
